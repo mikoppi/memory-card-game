4333bff6088bb4637a52f86156e419c965a0a164
Miko Peltokorpi <miko.peltokorpi@gmail.com>
2022-07-04 14:26:26 +0300

    Fix bug

--- src/components/Result.js
+++ src/components/Result.js
@@ -6,7 +6,7 @@ const Result = ( {currentScore, pokemonAmount, level, reset} ) => {
   return (
     <div className='result'>
         
-        {currentScore===1 ? 
+        {currentScore===24 ? 
         <h2 className='win'>
             <h1>You won!</h1>
             <button onClick={reset}>Play again</button>
